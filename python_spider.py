8784689e4a61665aacca48f101515c8bf83b07b8
cosystem <jougraw@gmail.com>
2015-10-30 19:17:26 +0100

    1. Add additional multithread through single page to extract images and roominfo (10 times faster than before). 2. use set instead of list to store tags to spead up Todo: 1. Higher level abstract: unify the two layers of multithread crawler 2. Check the connection stability issue 3. Modify the headers implementation

--- python_spider.py
+++ python_spider.py
@@ -7,10 +7,11 @@ import requests
 import re
 from bs4 import BeautifulSoup, SoupStrainer
 
-def get_single_item_data(item_url):
+def get_single_item_data(item_url, header):
     '''function to get the room images and info in each page'''
+    headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"}
     # get the page source and parse the code
-    source_code = requests.get(item_url)
+    source_code = requests.get(item_url, headers=headers)
     plain_text = source_code.text.encode('utf-8', 'ignore')
     images_and_info = SoupStrainer(['div', {'class': 'img01'}, 'script', {'type': 'text/javascript'}])
     soup = BeautifulSoup(plain_text, "html.parser", parse_only=images_and_info)
@@ -29,34 +30,45 @@ def get_single_item_data(item_url):
     roominfodict = dict(roominfolistnew)
     return (imglist, roominfodict)
 
-
 def get_rooms(page):
     '''function to get all the room information with specified page number'''
+    headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"}
     allimginfolist = []
+    tag_hosts = set()    # set to save all tags
     url = "http://zu.fang.com/house/"
     url = url + "i3" + str(page)
-    source_code = requests.get(url)
+    source_code = requests.get(url, headers=headers)
     plain_text = source_code.text
     titles = SoupStrainer('p',{'class': 'title'})
     soup = BeautifulSoup(plain_text, "html.parser", parse_only=titles)
     all_a_tag = soup.find_all('a', {'href': re.compile('.+')})
     all_a_tag.pop()
-    # for each link, spide into the link and get room info and all images
-    for link in all_a_tag:
-        href = "http://zu.fang.com" + link.get('href')
-        #print(href)
-        imginfotuple = get_single_item_data(href)
-        allimginfolist.append(imginfotuple)
+
+    # save all links in a page to a set data structure
+    for tag in all_a_tag:
+        href = "http://zu.fang.com" + tag.get('href')
+        tag_hosts.add(href)
+
+    # multithread process through the all links saved in the set
+    threadlist = []
+    for link in tag_hosts:
+        thread_host = Thread(target = get_single_item_data, args=(link,))
+        thread_host.start()
+        threadlist.append(thread_host)
+
+    # ensure all of the threads have finished
+    for j in threadlist:
+        j.join()
 
 # iterate through all pages and create thread for each page and append each thread to the infolist
 threadlist = []
-pagenumber = 1
+pagenumber = 2
 for i in range(1,pagenumber+1):
     thread_page = Thread(target=get_rooms, args=(i,))
     thread_page.start()
     threadlist.append(thread_page)
 
-# Ensure all of the threads have finished
+# ensure all of the threads have finished
 for j in threadlist:
     j.join()
 
