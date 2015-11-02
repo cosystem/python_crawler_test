8e2479be619b2483eb448c756c7b826f0956b7ac
cosystem <jougraw@gmail.com>
2015-11-02 18:50:17 +0000

    Control request speed to avoid block Modify: 1. use Multithread only in one page instead of through multipages 2. use list comprehension to store images urls instead of for loop -> list to optimize the speed Todo: 1. generate MultiThread class for higher level abstract 2. test with multi pages for block checking

--- python_spider.py
+++ python_spider.py
@@ -2,31 +2,36 @@
 __author__ = 'PGY'
 import timeit
 start = timeit.default_timer()
-from threading import Thread
+import threading
 import requests
 import re
+import socket
+import time
+import random, math
 from bs4 import BeautifulSoup, SoupStrainer
 
-def get_single_item_data(item_url, header):
+
+#socket.setdefaulttimeout(30)
+
+def get_single_item_data(item_url):
     '''function to get the room images and info in each page'''
     headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"}
     # get the page source and parse the code
-    source_code = requests.get(item_url, headers=headers)
+    source_code = requests.get(item_url, headers=headers, timeout=5)
     plain_text = source_code.text.encode('utf-8', 'ignore')
     images_and_info = SoupStrainer(['div', {'class': 'img01'}, 'script', {'type': 'text/javascript'}])
     soup = BeautifulSoup(plain_text, "html.parser", parse_only=images_and_info)
     # get all the links for the images
     all_imgs = soup.find_all('img', {'src': re.compile('.+'), 'name': 'ImgList'})
-    imglist = []
-    for img in all_imgs:
-        imglink = img.get('src')
-        imglist.append(imglink)
+    # get the image list
+    imglist = [img.get('src') for img in all_imgs]
     # get the room information
     allinfo = soup.find('script').get_text()
     roominfo = allinfo.split('var')[1]
     # process the data to save the roominfo properly in dictionary data structure
     roominfolist = roominfo.replace(' ','').replace(',','').replace('\'','').split('\r\n')[1:-3]
     roominfolistnew = [infoitem.split(":") for infoitem in roominfolist]
+    #print(roominfolistnew)
     roominfodict = dict(roominfolistnew)
     return (imglist, roominfodict)
 
@@ -37,7 +42,7 @@ def get_rooms(page):
     tag_hosts = set()    # set to save all tags
     url = "http://zu.fang.com/house/"
     url = url + "i3" + str(page)
-    source_code = requests.get(url, headers=headers)
+    source_code = requests.get(url, headers=headers, timeout=5)
     plain_text = source_code.text
     titles = SoupStrainer('p',{'class': 'title'})
     soup = BeautifulSoup(plain_text, "html.parser", parse_only=titles)
@@ -52,7 +57,7 @@ def get_rooms(page):
     # multithread process through the all links saved in the set
     threadlist = []
     for link in tag_hosts:
-        thread_host = Thread(target = get_single_item_data, args=(link,))
+        thread_host = threading.Thread(target = get_single_item_data, args=(link,))
         thread_host.start()
         threadlist.append(thread_host)
 
@@ -60,17 +65,20 @@ def get_rooms(page):
     for j in threadlist:
         j.join()
 
-# iterate through all pages and create thread for each page and append each thread to the infolist
-threadlist = []
-pagenumber = 2
-for i in range(1,pagenumber+1):
-    thread_page = Thread(target=get_rooms, args=(i,))
-    thread_page.start()
-    threadlist.append(thread_page)
+def main():
+    maxpage = 1
+    for page in range(1, maxpage+1):
+        get_rooms(page)
+        # to control the request speed
+        random_interval=random.randrange(1, 5, 1)
+        time.sleep(random_interval)
+
+if __name__=="__main__":
+    main()
 
 # ensure all of the threads have finished
-for j in threadlist:
-    j.join()
+# for j in threadlist:
+#    j.join()
 
 stop = timeit.default_timer()
 
